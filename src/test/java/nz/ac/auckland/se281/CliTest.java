a4252ee697a5b3ececc755f48e0b269fbe24f6c9
Saarthak Negi <negisarthak03@gmail.com>
2023-06-06 11:44:41 +1200

    Fixed clltTest timeout

--- src/test/java/nz/ac/auckland/se281/CliTest.java
+++ src/test/java/nz/ac/auckland/se281/CliTest.java
@@ -5,9 +5,14 @@ import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.rules.Timeout;
+
+
 import org.junit.Before;
 
 /** You must not change this class! */
@@ -45,7 +50,7 @@ public abstract class CliTest {
   }
 
   /** Timeout if test runs longer than 10 seconds */
-  // @Rule public Timeout timeout = new Timeout(10, TimeUnit.SECONDS);
+  @Rule public Timeout timeout = new Timeout(10, TimeUnit.SECONDS);
 
   /**
    * Configures the test output and input streams, by creating new temporary streams while storing
